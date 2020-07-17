6a5705d3755a7d044505ba681ecb7e30f5b58181
Looka997 <luca.marinelli997@gmail.com>
2020-07-17 19:12:35 +0200

    implements main

--- packages/src/Main.java
+++ packages/src/Main.java
@@ -1,5 +1,20 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Vector;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int i;
+        ConnectionPool connectionPool = new ConnectionPool();
+        String urls[] = {"https://unige.it/it/","https://2019.aulaweb.unige.it/my/aaaa"};
+        Vector<Future<Integer>> futures = new Vector(urls.length);
+        HashMap<String, Integer> responseCodes = new HashMap<String, Integer>();
+        for (i=0; i<urls.length; ++i)
+            futures.add(connectionPool.OpenConnection(urls[i]));
+        for (i=0; i<urls.length; ++i)
+            responseCodes.put(urls[i], futures.elementAt(i).get());
+        for (i=0; i<futures.capacity(); ++i)
+            System.out.println(urls[i] + " responded with " + responseCodes.get(urls[i]));
     }
 }
